bff700365063edfb445184005f341f1de23a3a5c
David Fu <fzysmile@gmail.com>
2016-07-19 13:48:34 -0700

    combin list files for the prob team works

--- combine.R
+++ combine.R
@@ -1,34 +1,60 @@
+
 cat1 = "/home/rsqsim-00/pub/longCats/UCERF3base35kyrs/eqs.UCERF3base.out"
 cat2 = "/home/rsqsim-00/pub/longCats/UCERF3base35kyr-80kyr/eqs.UCERF3base35kyr-80kyr.out"
 outName = "UCERF3base_80yrs"
 
 
 out1 = paste(outName, ".RData", sep="")
-out2 = paste(outName, ".eList", sep="")
-out3 = paste(outName, ".dList", sep="")
-out4 = paste(outName, ".tList", sep="")
-out5 = paste(outName, ".pList", sep="")
-
+out2 = paste(outName, ".eList", sep="") # int
+out3 = paste(outName, ".pList", sep="") # int
+out4 = paste(outName, ".dList", sep="") # double
+out5 = paste(outName, ".tList", sep="") # double
 
 eqFiles = c(cat1,cat2)
 print("start")
 eqs = readAndCombineEqfiles(eqFiles)
 
 
+### 1 .RData
 print("start save")
+print(str(eqs))
 #save(eqs, file = out1)
-#print(str(eqs))
 
 
+### 2 e
 print("start save list e")
-writeBin(eqs$eList, out2, size=4, endian = "big")
+x = as.integer(eqs$eList)
+con = file(out2,"wb")
+writeBin(x, con)
+close(con)
 
-print("start save list d")
-writeBin(eqs$dList, out3, size=8, endian = "big")
 
-print("start save list t")
-writeBin(eqs$tList, out4, size=8, endian = "big")
-
+### 3 p
 print("start save list p")
-writeBin(eqs$pList, out5, size=4, endian = "big")
+x = as.integer(eqs$pList)
+con = file(out3,"wb")
+writeBin(x, con)
+close(con)
 
+
+### 4 d
+print("start save list d")
+x = as.double(eqs$dList)
+con = file(out4,"wb")
+writeBin(x, con)
+close(con)
+
+
+### 5 t
+print("start save list t")
+x = as.double(eqs$tList)
+con = file(out5,"wb")
+writeBin(x, con)
+close(con)
+
+######## test
+print("test.....")
+print(readBin(file(out2, "rb"), what = "integer", n=10))
+print(readBin(file(out3, "rb"), what = "integer", n=10))
+print(readBin(file(out4, "rb"), what = "double", n=10))
+print(readBin(file(out5, "rb"), what = "double", n=10))
